8b8a477a7158b5cc5093a8c7f1c195e4d6e202e4
klauz42 <nikolai.androidrus@gmail.com>
2016-11-25 02:32:30 +0300

    Добавил задание 3a

--- scripts/iiia.sh
+++ scripts/iiia.sh
@@ -1,1 +1,5 @@
-#
+#!/bin/bash
+echo "Введите имя пользователя или пользователя или часть его имени:"
+read username
+un=$username
+awk -vVAR="$un" -F ":" '$1 ~VAR {print "Имя ползователя: "$1"; Хеш пароля: "$2 ";\n UID: "$3 "; GID: "$4 "; GECOS: "$5";\n Домашний каталог: "$6"; Shell: "$7"\n"} ' /etc/passwd
