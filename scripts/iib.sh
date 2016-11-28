c0be75b74cd24cb6f56462de559e4a47f2575684
rozaabdullaeva <rozaabdullaeva96@gmail.com>
2016-11-28 21:05:00 +0300

    2b

--- scripts/iib.sh
+++ scripts/iib.sh
@@ -1,1 +1,13 @@
-#
+#!/bin/bash
+
+#PS3='Выберите действие:'
+
+echo "Введите название группы, которую Вы хотите удалить (q - прекратить выполнение действия):"
+read groupname
+
+case $groupname in
+    q) echo "Выполнение действия отменено."
+       exit 0;;
+    *) groupdel $groupname
+       echo "Группа $groupname успешно удалена.";;
+esac
