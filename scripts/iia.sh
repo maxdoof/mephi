f7b2f27ff8b4f776f2613b00d423e4a182468266
rozaabdullaeva <rozaabdullaeva96@gmail.com>
2016-11-28 17:35:11 +0300

    2а

--- scripts/iia.sh
+++ scripts/iia.sh
@@ -1,1 +1,13 @@
-#
+#!/bin/bash
+
+PS3='Выберите действие:'
+
+echo "Введите название группы, которую Вы хотите добавить (q - прекратить выполнение действия):"
+read groupname
+
+case $groupname in
+    q) echo "Выполнение действия отменено."
+       exit 0;;
+    *) groupadd $groupname
+       echo "Группа $groupname успешно создана.";;
+esac
