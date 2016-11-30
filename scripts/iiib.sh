2205b1f6d72e9dc5d7eab1776ef8deb4ba3cd8f9
klauz42 <nikolai.androidrus@gmail.com>
2016-11-30 22:55:52 +0300

    Добавил задание 3b, исправил грамматические ошибки в 3a

--- scripts/iiib.sh
+++ scripts/iiib.sh
@@ -1,1 +1,29 @@
-#
+#!/bin/bash
+while [ "$gn" != "q" ]
+do
+	echo "Введите название группы или ее часть (help - помощь, q - выход):"
+	read gn
+	if [[ "$gn" == "q" ]]
+	then 
+		break	
+	elif [[ "$gn" == "help" ]]
+	then
+		echo "Справка: введите название группы или его часть, q - выход. Разработал klauz42" 
+	else
+		if [[ ${#gn} -ne 0 ]]
+		then
+			string="$(awk -vVAR="$gn" -F ":" '$1 ~VAR {print "Группа: "$1":\n GID: "$3"; Список участников: "$4"\n "} ' /etc/group)"
+		if [[ ${#string} -ne 0 ]]
+		then
+			IFS=""
+			echo $string | less 
+		else
+			echo "Совпадений не найдено"
+		fi
+		
+		else
+			echo "Вы ничего не ввели!" >&2
+		fi
+	fi
+done
+exit 0
