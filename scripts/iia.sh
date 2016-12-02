4bd9b2f72dbfaf268886063850d5eeb9dca7ea0b
rozaabdullaeva <rozaabdullaeva96@gmail.com>
2016-12-02 14:23:10 +0300

    2a доработанный

--- scripts/iia.sh
+++ scripts/iia.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-PS3='Выберите действие:'
+while [ "$groupname" != "q" ]
+do
+    
+    #PS3='Выберите действие:'
 
-echo "Введите название группы, которую Вы хотите добавить (q - прекратить выполнение действия):"
-read groupname
+    echo "Введите название группы, которую Вы хотите добавить (q - прекратить выполнение действия):"
+    read groupname
+    
+    if [[ "$groupname" == "q" ]]
+    then
+	break
+    elif [[ "$groupname" == "help" ]]
+    then
+	echo "Вам необходимо ввести имя группы, которую вы хотите добавить"
+    else
+	if [[ ${#groupname} -ne 0 ]]
+	then
+	    #здесь нужна проверка на существование группы
+	    groudadd $groupname
+	    echo "Группа $groupname успешно создана"
+	else 
+	    echo "Вы не ввели имя группы!" >&2
+	fi
+    fi
 
-case $groupname in
-    q) echo "Выполнение действия отменено."
-       exit 0;;
-    *) groupadd $groupname
-       echo "Группа $groupname успешно создана.";;
-esac
+done
+exit 0
