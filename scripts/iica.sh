23befde3ed6ac5e9131ed1f53a6b1779cd55863f
root <root@localhost.localdomain>
2016-12-09 09:26:34 +0300

    исправил

--- scripts/iica.sh
+++ scripts/iica.sh
@@ -15,31 +15,42 @@ do
 
     if [[ "$groupname" == "help" ]]
     then
-	echo "Вам необходимо ввести имя группы, в которую Вы хотите добавить пользователя."
+	echo "Вам необходимо ввести имя группы , в которую Вы хотите добавить пользователя и позже номер пользователя."
 
     
     elif [[ $is_group != "true" ]]
     then
 	echo "Группа с таким названием не существует"
+	echo
 
     else
-        echo "Введите  имя пользователя, которого Вы хотите добавить в эту группу (q - прекратить выполнение действия):"
-        read username
-        if [[ "$username" == "q" ]]
+	cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1}' | cat -n
+	echo
+        echo "Введите  номер пользователя, которого Вы хотите добавить в эту группу (q - прекратить выполнение действия):"
+        read choice key
+	user=$(cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1 }' | cat -n | awk '($1=="'$choice'"){print $2}')
+	if [[ "$choice" == "help" ]]
         then
+	    echo "Вам необходимо ввести имя группы , в которую Вы хотите добавить пользователя и позже номер пользователя"
+	    break	
+	fi
+
+	if [ "$choice" == "q" ]
+	then
 	    break
 	fi
-        if [[ "$username" == "help" ]]
-        then
-	    echo "Вам необходимо ввести имя пользователя, которого Вы хотите добавить."
-        
+
+        if [ "$user" == "" ]
+	then
+	    echo Ошибка! Пользователь не найден >&2
+	    echo
     
 	elif [[ ${#groupname} -ne 0 ]]
 	then
-	    usermod -a -G $groupname $username
+	    usermod -a -G $groupname $user
 	    tail -5 /etc/group
 	else 
-	    echo "Вы не ввели название группы или имя пользователя!" >&2
+	    echo "Вы не ввели имя группы!" >&2
 	fi
     fi
 done
