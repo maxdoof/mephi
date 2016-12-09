001569453160be10376dd5364e57e493af7a44d0
root <root@localhost.localdomain>
2016-12-09 05:10:45 +0300

    изминил

--- scripts/iia.sh
+++ scripts/iia.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+
+
+
 while [ "$groupname" != "q" ]
 do
     
     #PS3='Выберите действие:'
 
+
+
+
+    #выводит группы, которые уже есть
+    #echo "Cуществующие группы:"
+    #echo "--------------------"
+    #tail -5 /etc/group
+    #echo "--------------------"
+
     echo "Введите название группы, которую Вы хотите добавить (q - прекратить выполнение действия):"
     read groupname
-    
+
+    is_group=$(cat /etc/group | cut -d : -f1 | awk '($1=="'$groupname'"){print "true"}')
+
+    if [[ $is_group == "true" ]]
+    then
+
+	echo "Группа с таким названием уже существует"
+	echo "Введите снова название группы"
+        read groupname
+    fi
+
     if [[ "$groupname" == "q" ]]
     then
 	break
@@ -19,8 +41,9 @@ do
 	then
 	    #здесь нужна проверка на существование группы
 	    groupadd $groupname
+	    echo "Последние созданные группы"
 	    tail -5 /etc/group
-	    # echo -e "Группа $groupname успешно создана.\n"
+	    echo -e "Группа $groupname успешно создана.\n"
 	else 
 	    echo "Вы не ввели имя группы!" >&2
 	fi
