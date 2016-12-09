7641c7d18d35c9e5d96b2ab41bd93a529931cec4
Дмитрий Елькин <erikssoon@mail.ru>
2016-12-09 03:43:31 +0300

    работает iicb

--- scripts/iicb.sh
+++ scripts/iicb.sh
@@ -1,1 +1,38 @@
-#
+##!/bin/bash
+
+while [ "$groupname" != "q" ]
+do
+    #PS3='Выберите действие:'
+    echo "Введите группу,в которой Вы хотите  удалить пользователя(q - прекратить выполнение действия):"
+    read groupname
+    if [[ "$groupname" == "q" ]]
+    then
+	break
+    fi
+    if [[ "$groupname" == "help" ]]
+    then
+	echo "Вам необходимо ввести название группы,в которой вы хотите удалить пользователя"
+    fi
+    echo "Введите  имя пользователя, которого Вы хотите удалить из $groupname(q - прекратить выполнение действия):"
+    read username
+    if [[ "$groupname" == "q" ]]
+    then
+	break
+    elif [[ "$groupname" == "help" ]]
+    then
+	echo "Вам необходимо ввести имя пользователя,которого вы хотите удалить"
+    else
+	if [[ ${#groupname} -ne 0 ]]
+	then
+	    gpasswd -d $username $groupname
+	    tail -5 /etc/group
+	else 
+	    echo "Вы не ввели название группы или имя пользователя!" >&2
+	fi
+    fi
+
+
+
+done
+
+exit 0
