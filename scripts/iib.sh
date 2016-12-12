8f6da1d251c7fbd1ddfcb23b2da252c4ac9c3080
rozaabdullaeva <rozaabdullaeva96@gmail.com>
2016-12-12 17:41:42 +0300

    Цикл добавила

--- scripts/iib.sh
+++ scripts/iib.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-#PS3='Выберите действие:'
-echo "Введите название группы, которую Вы хотите удалить (q - прекратить выполнение действия):"
-read groupname
+while [ "$groupname" != "q" ]
+do
 
-case $groupname in
-    q) echo "Выполнение действия отменено."
-       exit 0;;
-    *) groupdel $groupname
-       echo "Группа $groupname успешно удалена.";;
-esac
+    #PS3='Выберите действие:'
+    echo "Введите название группы, которую Вы хотите удалить (q - прекратить выполнение действия):"
+    read groupname
+    if [[ "$groupname" == "q"]]
+    then
+	break
+    elif [["$groupname" == help ]]
+    then
+	echo "Введите имя группы, которую Вы хотите удалить."
+    else
+	if [[ ${#groupname} -ne 0 ]]
+        then
+	    groupdel $groupname
+            echo "Группа $groupname успешно удалена.";;
+done
+exit 0
